d034b496d9b6ff9a165e84af759566d1f4c04d5c
Vlad Hmara <hmara.vladislav2@gmail.com>
2021-04-07 16:12:53 +0300

    fix test config

--- src/testing/config.js
+++ src/testing/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  username: LT_USERNAME,
-  key: LT_KEY,
+  username: process.env.LT_USERNAME,
+  key: process.env.LT_KEY,
   gridHost: 'hub.lambdatest.com/wd/hub',
   credentials: {
     email: process.env.LB_EMAIL,
